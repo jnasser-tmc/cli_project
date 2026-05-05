832d0247f432fb78573a0e80d2e194b451f3e431
jnasser-tmc <jnasser@tmc.edu>
2026-05-05 15:28:10 -0500

    section 2 lessons

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,5 @@
+# mcp_server.py
+
 from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
@@ -46,14 +48,25 @@ def edit_document(
 
 ## Write a resource to return all doc id's
 @mcp.resource(
-    "doc://documents",
+    "docs://documents", # Direct Resource: Static URI
     mime_type="application/json",
 )
 def list_docs() -> list[str]:
     return list(docs.keys())
 
+## Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents/{doc_id}", # Templated Resource: URI with path parameter
+    mime_type="text/plain",
+)
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    
+    return docs[doc_id]
 
-# TODO: Write a resource to return the contents of a particular doc
+
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
